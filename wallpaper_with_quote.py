4af05e8d3f2d06d5a245ad7224bcd5f23b4f9364
orujahmadov <oruc888@yahoo.com>
2016-12-14 22:43:38 -0500

    minor_changes

--- wallpaper_with_quote.py
+++ wallpaper_with_quote.py
@@ -6,6 +6,14 @@ import urllib
 import string
 import random
 from random import randint
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+import unirest
+# Just importing Forsimatic class
+from forismatic import Forismatic
+from AppKit import NSScreen
 
 def set_desktop_background(filename):
     subprocess.Popen(SCRIPT%filename, shell=True)
@@ -34,3 +42,15 @@ image_name = "/Users/orujahmadov/Desktop/500PX/" + image_name + ".jpg"
 random_index = randint(0,len(response["photos"]))
 urllib.urlretrieve(response["photos"][random_index]["images"][0]["url"], image_name)
 set_desktop_background(image_name)
+
+font = ImageFont.truetype("/Users/orujahmadov/Downloads/open_sans/OpenSans-Regular.ttf", 35)
+img = Image.open(image_name)
+draw = ImageDraw.Draw(img)
+forismatic = Forismatic()
+quotes = forismatic.get_quote()
+W, H = (800,1200)
+w, h = draw.textsize(quotes.quote)
+draw.text(((W-w)/2,(H-h)/2), "\"" + quotes.quote + "\"", (250,250,250), font=font)
+img.save("/Users/orujahmadov/Desktop/test.jpg")
+print(NSScreen.mainScreen().frame().size.width)
+print(NSScreen.mainScreen().frame().size.height)
