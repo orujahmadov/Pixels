d6e6da948f3dc9533815500a9ce7dc13586ed63d
orujahmadov <oruc888@yahoo.com>
2017-01-21 15:21:29 -0500

    ui_changes

--- pixels.py
+++ pixels.py
@@ -11,6 +11,7 @@ import time
 import Tkinter
 from Tkinter import *
 from tkMessageBox import *
+import ttk
 
 ############## CONSTANT VALUES ################################################
 
@@ -18,7 +19,7 @@ CONSUMER_KEY = 'OSdV70a94YN4ccIg2nIgUHQQV5tiLqVY4KrkREgQ'
 CONSUMER_SECRET= 'LNFCMtkbpJwW5GHoj3ezhWNsIKWALfB22SQOxQZi'
 CATEGORY_NAMES = ["Uncategorized","Abstract","Animals","Black and White","Celebrities","City and Architecture","Commercial","Concert","Family","Fashion","Film","Fine Art","Food","Journalism","Landscapes","Macro","Nature","Nude","People","Performing Arts","Sport","Still Life","Street","Transportation New!","Travel","Underwater","Urban Exploration New!","Wedding New!"]
 FEATURE_NAMES = ["Popular","Editors","Upcoming","Fresh Today","Fresh Yesterday","Fresh Week"]
-INTERVAL_NAMES = ["Every 5 seconds","Every minute","Every 5 minutes ","Every 15 minutes","Every 30 minutes","Every hour","Every day"]
+INTERVAL_NAMES = ["Every 5 seconds","Every minute","Every 5 minutes","Every 15 minutes","Every 30 minutes","Every hour","Every day"]
 
 ############### DICTIONARIES #################################################
 INTERVAL_DICTIONARY = {"Every 5 seconds":5.0, "Every minute":60.0, "Every 5 minutes":300.0, "Every 15 minutes":900.0, "Every 30 minutes":1800, "Every hour":3600.0, "Every day":86400.0}
@@ -64,7 +65,7 @@ class Pixels(Tkinter.Tk):
         label_feature = Tkinter.Label(self,text="Feature")
         label_feature.grid(column=0,row=1,sticky=E)
         self.option_feature = OptionMenu(self, self.feature, *FEATURE_NAMES)
-        self.option_feature.grid(column=1,row=1,sticky=W)
+        self.option_feature.grid(column=1,row=1,sticky=EW, padx=5, pady=5)
 
         self.category = Tkinter.StringVar()
         self.category.set('Uncategorized') #Default value
@@ -72,7 +73,7 @@ class Pixels(Tkinter.Tk):
         label_category = Tkinter.Label(self,text="Category")
         label_category.grid(column=0,row=2,sticky=E)
         self.option_category = OptionMenu(self, self.category, *CATEGORY_NAMES)
-        self.option_category.grid(column=1,row=2,sticky=W)
+        self.option_category.grid(column=1,row=2,sticky=EW, padx=5, pady=5)
 
         self.interval = Tkinter.StringVar()
         self.interval.set('Every hour') #Default value
@@ -80,13 +81,20 @@ class Pixels(Tkinter.Tk):
         label_interval = Tkinter.Label(self,text="Interval")
         label_interval.grid(column=0,row=3,sticky=E)
         self.option_interval = OptionMenu(self, self.interval, *INTERVAL_NAMES)
-        self.option_interval.grid(column=1,row=3,sticky=W)
+        self.option_interval.grid(column=1,row=3,sticky=EW, padx=5, pady=5)
 
         self.button = Tkinter.Button(self,text=u"New Wallpaper", command=self.OnButtonClick)
-        self.button.grid(column=1,row=4,sticky=W)
+        self.button.grid(column=1,row=4,sticky=EW, padx=5, pady=5)
 
         self.grid_columnconfigure(0,weight=1)
         self.grid_rowconfigure(0,weight=1)
+
+        self.progress = ttk.Progressbar(self, orient="horizontal", length=200, mode="determinate")
+        self.image_index = 0
+        self.image_max_index = 100
+        self.progress.grid(column=1,row=5,sticky=EW, padx=5, pady=10)
+
+        self.response = ""
 
     def OnButtonClick(self):
         selected_feature = self.feature.get().replace(" ","_").lower()
@@ -110,21 +118,33 @@ class Pixels(Tkinter.Tk):
     def update_wallpaper(self, feature, category):
         handler = self.authorization_url_with_verifier()
         api = FiveHundredPXAPI(handler)
-        response = json.loads(json.dumps(api.photos(require_auth=True, feature=feature, only=category, rpp=100, image_size=2048)))
-        if len(response["photos"]) == 0:
+        self.response = json.loads(json.dumps(api.photos(require_auth=True, feature=feature, only=category, rpp=100, image_size=2048)))
+        if len(self.response["photos"]) == 0:
             self.error()
         else:
             ensure_dir_valid()
             clean_directory()
-            image_name = ""
-            for index in range(len(response["photos"])-1):
-                image_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
-                image_name = DIRECTORY + image_name + ".jpg"
-                urllib.urlretrieve(response["photos"][index]["images"][0]["url"], image_name)
+            image_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
+            image_name = DIRECTORY + image_name + ".jpg"
+            urllib.urlretrieve(self.response["photos"][0]["images"][0]["url"], image_name)
             self.set_desktop_background(image_name)
+            self.image_index = 0
+            self.progress["value"] = 0
+            self.progress["maximum"] = 100
+            self.download_images()
+
+    def download_images(self):
+        self.image_index += 1
+        self.progress["value"] = self.image_index
+        if self.image_index < self.image_max_index:
+            image_name = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(40))
+            image_name = DIRECTORY + image_name + ".jpg"
+            urllib.urlretrieve(self.response["photos"][self.image_index]["images"][0]["url"], image_name)
+            self.after(100, self.download_images)
 
 
 if __name__ == "__main__":
     pixels = Pixels(None)
     pixels.title('Pixels')
+    pixels.resizable(width=False, height=False)
     pixels.mainloop()
